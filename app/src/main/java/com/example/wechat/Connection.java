b0eb982afe9839b7a55e49e8ac2334318b25ccbc
pcClase <ikadh@plaiaundi.net>
2022-12-02 10:32:10 +0100

    2

--- app/src/main/java/com/example/wechat/Connection.java
+++ app/src/main/java/com/example/wechat/Connection.java
@@ -4,97 +4,83 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class Connection {
+    MainActivity mainActivity;
+    Socket socket;
+    HandlerThread handlerThread, handlerThread2, handlerThread3;
+    Handler handler, handler2, handler3;
+    Looper looper, looper2, looper3;
+    public Connection(MainActivity mainActivity){
+        this.mainActivity = mainActivity;
+    }
 
-    public void run() {
+    public void connect() {
         try {
 //          Socket
-            HandlerThread handlerThread = new HandlerThread("Socket");
+            handlerThread = new HandlerThread("Socket");
             handlerThread.start();
-            Looper looper = handlerThread.getLooper();
-            Handler handler = new Handler(looper);
+            looper = handlerThread.getLooper();
+            handler = new Handler(looper);
             handler.post(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        Socket socket = new Socket("localhost", 6666);
+                        socket = new Socket("localhost", 6666);
+//          BufferedReader
+                        handlerThread2 = new HandlerThread("BufferedReader");
+                        handlerThread2.start();
+                        looper2 = handlerThread2.getLooper();
+                        handler2 = new Handler(looper2);
+
+//          BufferedWriter
+                        handlerThread3 = new HandlerThread("BufferedWriter");
+                        handlerThread3.start();
+                        looper3 = handlerThread3.getLooper();
+                        handler3 = new Handler(looper3);
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             });
-
-//          BufferedReader
-            HandlerThread handlerThread2 = new HandlerThread("BufferedReader");
-            handlerThread2.start();
-            Looper looper2 = handlerThread2.getLooper();
-            Handler handler2 = new Handler(looper2);
-            handler2.post(new Runnable() {
-                @Override
-                public void run() {
-
-                }
-            });
-
-//          BufferedWriter
-            HandlerThread handlerThread3 = new HandlerThread("BufferedWriter");
-            handlerThread3.start();
-            Looper looper3 = handlerThread3.getLooper();
-            Handler handler3 = new Handler(looper3);
-            handler3.post(new Runnable() {
-                @Override
-                public void run() {
-
-                }
-            });
-
-//            TODO: Creacion Socket
-//            Socket socket = new Socket("localhost", 6666);
-
-//            TODO: Aceptacion del socket
-//            ServerSocket serverSocket = new ServerSocket(6666);
-//            Socket cliente = serverSocket.accept();
-
-
-            /*
-            InputStream inputStream = cliente.getInputStream();
-
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String linea;
-            while ((linea = bufferedReader.readLine()) != null){
-                String lineaFinal = "Cliente " + contador +" escribe: " + linea;
-                System.out.println(lineaFinal);
-                main.listSockets.forEach(socket -> {
-                    try {
-                        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                        bufferedWriter.write(lineaFinal);
-                        bufferedWriter.newLine();
-                        bufferedWriter.flush();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            }*/
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public void createSocket(){
-
-    }
-
-    public void sendMessages(){
-
-    }
-
     public void getMessages(){
 
+        handler2.post(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
     }
 
+    public void sendMessages(String mensaje){
+        handler3.post(new Runnable() {
+            @Override
+            public void run() {
+                if(mensaje.trim().length() > 0){
+                    BufferedWriter bufferedWriter = null;
+                    try {
+                        bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        bufferedWriter.write(mensaje);
+                        bufferedWriter.newLine();
+                        bufferedWriter.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+    }
 
 
 }
--- app/src/main/java/com/example/wechat/MainActivity.java
+++ app/src/main/java/com/example/wechat/MainActivity.java
@@ -3,19 +3,34 @@ package com.example.wechat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mensajes;
+    Button btnEnviar;
+    EditText mensajeEscrito;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Connection connection = new Connection();
+        btnEnviar = findViewById(R.id.btnEnviar);
+        mensajes = findViewById(R.id.idMensajes);
+        mensajeEscrito = findViewById(R.id.editTxtTexto);
 
-
-
+        Connection connection = new Connection(this);
+        connection.connect();
+        btnEnviar.setOnClickListener(view -> {
+            Log.i("daasd", mensajeEscrito.getText().toString());
+            connection.sendMessages(mensajeEscrito.getText().toString());
+        });
     }
 }
